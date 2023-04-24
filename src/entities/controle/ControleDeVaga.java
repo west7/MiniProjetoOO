2d69db99c4ce0e4bf0acb7250ffb4a863db4a29b
Guilherme Westphall <guilhermewestphall@gmail.com>
2023-04-24 12:51:30 -0300

    Metodos para buscar empresas e organizar salarios

--- src/entities/controle/ControleDeVaga.java
+++ src/entities/controle/ControleDeVaga.java
@@ -1,7 +1,10 @@
 package entities.controle;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
+import entities.Empresa;
 import entities.Vaga;
 import entities.enums.VagaStatus;
 
@@ -24,6 +27,29 @@ public class ControleDeVaga {
 	
 	public void removeVaga(Vaga vaga) {
 		this.vagas.remove(vaga);
+	}
+	
+	public ArrayList<Vaga> BuscarPorEmpresa(String nomeEmpresa){
+		ArrayList<Vaga> vagasFiltradas = new ArrayList<>();
+		for(Vaga v : vagas) {
+			Empresa empresa = v.getEmpresa();
+			if (empresa != null && empresa.getNome().equals(nomeEmpresa)) {
+				vagasFiltradas.add(v);
+			}
+		}
+		return vagasFiltradas;
+	}
+	
+	public ArrayList<Vaga> OrdenarPeloSalario(){
+		ArrayList<Vaga> vagasOrdenadasSalario = new ArrayList<>(vagas);
+		Collections.sort(vagasOrdenadasSalario, new Comparator<Vaga>() {
+			@Override
+			public int compare(Vaga v1, Vaga v2) {
+				return Double.compare(v2.getSalario(), v1.getSalario()); //Organizando em ordem Decrescente
+				
+			}
+		});
+		return vagasOrdenadasSalario;
 	}
 	
 }
